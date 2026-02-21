b2baaeb97d9e16d273dbb8e8c0fdbb4f1a00e001
Edwin <ewn.francisco@gmail.com>
2026-02-21 16:14:52 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../Components/Avatar'
+import Paragrafo from '../../Components/Paragrafo'
 import Title from '../../Components/Title'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ewn1.png" />
-    <Title fontSize={20}>Edwin Francisco</Title>
+    <SidebarContainer>
+      <Avatar />
+      <Title fontSize={20}>Edwin Francisco</Title>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        ewn1
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
